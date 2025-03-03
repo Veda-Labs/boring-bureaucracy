f9231feef6bc84dc724fb5976bee9f9486a48052
crispymangoes <crispymangoes@gmail.com>
2025-03-03 14:53:12 -0800

    MVP seems to work

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,18 +1,31 @@
+use alloy::primitives::{Bytes, U256};
+use alloy::{sol, sol_types::SolCall};
 use dotenv::dotenv;
 use eyre::Result;
+use hex;
 use reqwest::Client;
 use serde::Deserialize;
 use serde_json::json;
 use std::{env, fs};
 
+sol! {
+    #[sol(rpc)]
+    contract GnosisSafe {
+        function execTransactionFromModule(address to, uint256 value, bytes memory data, uint8 operation);
+    }
+}
+
 #[derive(Deserialize)]
 struct SimulationConfig {
     network_id: String,
+    multisig: String,
     to: String,
-    input: String,
+    value: String,
+    data: String,
+    operation: String,
 }
 
-fn read_simulation_config(file_path: &str) -> Result<SimulationConfig, Box<dyn std::error::Error>> {
+fn read_simulation_config(file_path: &str) -> Result<SimulationConfig> {
     let file_content = fs::read_to_string(file_path)?;
     let config: SimulationConfig = serde_json::from_str(&file_content)?;
     Ok(config)
@@ -25,31 +38,50 @@ pub async fn run_simulation() -> Result<()> {
     let account_slug = env::var("TENDERLY_ACCOUNT_SLUG")?;
     let project_slug = env::var("TENDERLY_PROJECT_SLUG")?;
 
-    let client = Client::new();
-    let response = client.post(&format!(
-        "https://api.tenderly.co/api/v1/account/{}/project/{}/simulate",
-        account_slug, project_slug
-    ))
-    .header("X-Access-Key", api_key)
-    .json(&json!({
-        "save": true,
-        "save_if_fails": true,
-        "simulation_type": "full",
-        "network_id": "1",
-        "from": "0xe2e2e2e2e2e2e2e2e2e2e2e2e2e2e2e2e2e2e2e2",
-        "to": "0x6b175474e89094c44da98b954eedeac495271d0f",
-        "input": "0x40c10f19000000000000000000000000e58b9ee93700a616b50509c8292977fa7a0f8ce10000000000000000000000000000000000000000000000001bc16d674ec80000",
-        "gas": 8000000,
-        "state_objects": {
-            "0x6b175474e89094c44da98b954eedeac495271d0f": {
-                "storage": {
-                    "0xedd7d04419e9c48ceb6055956cbb4e2091ae310313a4d1fa7cbcfe7561616e03": "0x0000000000000000000000000000000000000000000000000000000000000001"
-                }
+    let config = read_simulation_config("admin_tx.json")?;
+
+    let from_address = "0xe2e2e2e2e2e2e2e2e2e2e2e2e2e2e2e2e2e2e2e2";
+    // TOOD should chagne state of config.multisig
+    let state_objects = json!({
+        config.multisig.clone(): {
+            "storage": {
+                "0xd71a90a935e1abe19645d4f9630a0044413a815e634f2ca5c4b4b04becfec14c": "0x0000000000000000000000000000000000000000000000000000000000000001"
             }
         }
-    }))
-    .send()
-    .await?;
+    });
+
+    let client = Client::new();
+
+    // Build input.
+    let input = GnosisSafe::execTransactionFromModuleCall::new((
+        config.to.parse().expect("Failed to parse to"),
+        U256::from(config.value.parse::<U256>().expect("Failed to parse value")),
+        Bytes::from(config.data.parse::<Bytes>().expect("Failed to parse data")),
+        config.operation.parse().expect("Failed to parse operation"),
+    ))
+    .abi_encode();
+
+    let input_hex = hex::encode(input);
+
+    let response = client
+        .post(&format!(
+            "https://api.tenderly.co/api/v1/account/{}/project/{}/simulate",
+            account_slug, project_slug
+        ))
+        .header("X-Access-Key", api_key)
+        .json(&json!({
+            "save": true,
+            "save_if_fails": true,
+            "simulation_type": "full",
+            "network_id": config.network_id,
+            "from": from_address,
+            "to": config.multisig,
+            "input": input_hex,
+            "gas": 10_000_000,
+            "state_objects": state_objects,
+        }))
+        .send()
+        .await?;
 
     let simulation_result = response.json::<serde_json::Value>().await?;
 
