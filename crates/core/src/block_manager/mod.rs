e72eda4b1992dacc06fe67358167a6cf40bc0196
crispymangoes <crispymangoes@gmail.com>
2025-05-06 10:33:24 -0700

    Add proc macro

--- crates/core/src/block_manager/mod.rs
+++ crates/core/src/block_manager/mod.rs
@@ -1,2 +1,3 @@
 pub mod block_manager;
 pub mod building_blocks;
+pub mod example;
---
+++ crates/into_trait/src/lib.rs
@@ -0,0 +1,49 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Attribute, Data, DataEnum, DeriveInput, Path};
+
+#[proc_macro_derive(IntoTraitObject, attributes(trait_name))]
+pub fn derive_into_trait_object(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let enum_name = &input.ident;
+
+    // Find the trait_name attribute
+    let mut trait_name: Option<proc_macro2::TokenStream> = None;
+    for attr in &input.attrs {
+        if attr.path().is_ident("trait_name") {
+            attr.parse_nested_meta(|meta| {
+                if let Some(path) = meta.path.get_ident() {
+                    trait_name = Some(quote! { #path });
+                }
+                Ok(())
+            })
+            .unwrap();
+        }
+    }
+    let trait_name = trait_name.expect("You must specify #[trait_name(TraitName)] on the enum when using #[derive(IntoTraitObject)]");
+
+    let Data::Enum(DataEnum { variants, .. }) = &input.data else {
+        panic!("IntoTraitObject can only be derived for enums");
+    };
+
+    let arms = variants.iter().map(|v| {
+        let vname = &v.ident;
+        quote! {
+            #enum_name::#vname(data) => Box::new(data) as Box<dyn #trait_name>
+        }
+    });
+
+    let expanded = quote! {
+        impl #enum_name {
+            pub fn into_trait_object(self) -> Box<dyn #trait_name> {
+                match self {
+                    #(#arms),*
+                }
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
